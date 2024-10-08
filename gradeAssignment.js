66d936eb621a7fcd1d90f563a4637b3a11d293e2
afquidangen <alyssafayequidangen@gmail.com>
2024-10-08 21:28:00 +0800

    If Else If Activity

---
+++ gradeAssignment.js
@@ -0,0 +1,14 @@
+function askScore() {
+    let score = prompt("Enter your score:");
+    if (score >= 90) {
+        alert("Excellent.");
+    } else if (score >= 80 && score <=89){
+        alert("Good.");
+    } else if (score >= 70 && score <=79) {
+        alert("Fair.");
+    } else if (score < 70 && score > 0) {
+        alert("Needs Improvement.");
+    } else {
+        alert("Invalid input.")
+    }
+}
